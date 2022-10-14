230a5e068964b6f2e52fa88d9899f5c60f34a684
GibberishDev <gibberishDevelopment@gmail.com>
2022-10-14 23:27:16 +0300

    added sidebar visual output for command

--- src/main/java/com/gibbdev/server/commands/TestSidebarSetup.java
+++ src/main/java/com/gibbdev/server/commands/TestSidebarSetup.java
@@ -1,30 +1,27 @@
 package com.gibbdev.server.commands;
 
-import com.gibbdev.SculkAlram;
+import com.gibbdev.client.SidebarDisplay;
 import de.tr7zw.nbtapi.NBTEntity;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.logging.Level;
 
 public class TestSidebarSetup implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(label + " can't be run in console");
             return true;
         }
         Player player = ((Player) sender).getPlayer();
+        assert player != null;
         NBTEntity playerNBT = new NBTEntity(player);
         Integer warningLevel = playerNBT.getCompound("warden_spawn_tracker").getInteger("warning_level");
         Integer ticksSinceLastWarning = playerNBT.getCompound("warden_spawn_tracker").getInteger("ticks_since_last_warning");
-        Integer ticksOfWarningTruceRemaining =  playerNBT.getCompound("warden_spawn_tracker").getInteger("cooldown_ticks");
-        sender.sendMessage("Alarm level: " + warningLevel + ", Seconds until level decrease: " + Math.round((12000 - ticksSinceLastWarning) * 0.05));
-
+        SidebarDisplay.updateAlarmSidebar(player, warningLevel, (int) Math.round((12000 - ticksSinceLastWarning) * 0.05));
         return true;
     }
 }
